8789a32e33c7ac024b8846caf30c14d3a04f5e2f
shubham bhoj <shubham.bhoj@binaryhat.in>
2023-02-08 13:16:57 +0530

    Adding project SB06

---
+++ SB06/api/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import render
+from api.models import Student
+from api.serializers import StudentSerializer
+import io
+from django.views import View 
+from rest_framework.parsers import JSONParser
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class StudentAPI(View):
+    def get(self,request,*args,**kwargs):
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id',None)
+        if id is not None:
+            stu=Student.objects.get(id=id)
+            ser=StudentSerializer(stu)
+            return JsonResponse(ser.data, safe=True)
+        stu=Student.objects.all()
+        ser=StudentSerializer(stu,many=True)
+        return JsonResponse(ser.data, safe=False)
+    
+    def post(self,request,*args, **kwargs):
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        ser=StudentSerializer(data=pythondata)
+        if ser.is_valid():
+            ser.save()
+            res={'msg':"data inserted successfully"}
+            return JsonResponse(res,safe=False)
+        return JsonResponse(ser.errors,safe=False)
+
+    def put(self,request,*args, **kwargs):
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id')
+        stu=Student.objects.get(id=id)
+        ser=StudentSerializer(stu,data=pythondata,partial=True)
+        if ser.is_valid():
+            ser.save()
+            res={'msg':'data update successfully!!!'}
+            return JsonResponse(res, safe=False)
+
+        return JsonResponse(ser.errors, safe=False)
+
+    def delete(self,request,*args, **kwargs):
+        json_data=request.body
+        stream=io.BytesIO(json_data)
+        pythondata=JSONParser().parse(stream)
+        id=pythondata.get('id')
+        stu=Student.objects.get(id=id)
+        stu.delete()
+        res={'msg':'data deleted successfully!!'}
+        return JsonResponse(res, safe=False)
+
+
+
