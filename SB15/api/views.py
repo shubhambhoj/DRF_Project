b01cc16e129bcd1c1fd5b4c2eacc319503b2b0f1
shubham bhoj <shubham.bhoj@binaryhat.in>
2023-02-14 14:08:15 +0530

    Updating project SB15

---
+++ SB15/api/views.py
@@ -0,0 +1,42 @@
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework.generics import ListAPIView,CreateAPIView,RetrieveAPIView,UpdateAPIView,DestroyAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateAPIView,RetrieveDestroyAPIView,RetrieveUpdateDestroyAPIView
+
+class StudentList(ListAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentCreate(CreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentRetrieve(RetrieveAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentUpdate(UpdateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentDestroy(DestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentListCreate(ListCreateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentRetrieveUpdate(RetrieveUpdateAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentRetrieveDestroy(RetrieveDestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+class StudentRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+
