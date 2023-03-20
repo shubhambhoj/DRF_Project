e3cb0f7d3edac862f1e22962c49a945870a28733
shubham bhoj <shubham.bhoj@binaryhat.in>
2023-03-20 15:35:23 +0530

    adding project SB24

---
+++ SB24/SB24/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path,include
+from api.views import StudentModelViewSet
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('studentapi', StudentModelViewSet, basename='studentapi')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path('auth/',include('rest_framework.urls'))
+]
